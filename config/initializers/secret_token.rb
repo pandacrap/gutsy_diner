400f1c5f99ad8acca43ae1f39f1d1bfffad4fc9f
Stephen Yu <stephen.jk.yu@outlook.com>
2014-03-12 22:10:50 -0600

    edit secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-GutyDiner::Application.config.secret_key_base = 'ddd25e3c62a6d3d4d2322ccaf0aa331c0b4b288d6dc214e26fd945eb8664957823c77f4864b253bfe29ee026aae6afd1fa10a16ee0e48d7de4a5ee5f2b97138e'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+GutyDiner::Application.config.secret_key_base = secure_token
