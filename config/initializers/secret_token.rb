74170624bc7dcb47cc6157f925d223543c4d57df
Stephen Yu <stephen.jk.yu@outlook.com>
2014-03-12 20:16:35 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GutyDiner::Application.config.secret_key_base = 'ddd25e3c62a6d3d4d2322ccaf0aa331c0b4b288d6dc214e26fd945eb8664957823c77f4864b253bfe29ee026aae6afd1fa10a16ee0e48d7de4a5ee5f2b97138e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GutyDiner::Application.config.session_store :cookie_store, key: '_guty_diner_session'
