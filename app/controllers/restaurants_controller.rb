e64a1d0fde26dcc67b70294ae052e0a2cf52666a
Stephen Yu <stephen.jk.yu@outlook.com>
2014-03-31 20:44:34 -0600

    add multiple results and floating div

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,19 +11,22 @@ class RestaurantsController < ApplicationController
   
   def show    
     client = Foursquare2::Client.new(:client_id => 'IGHYS4WU2EMECJYBR5E3YLPVOEKWLVA4JUQFJHG4Q1A3IGOF', :client_secret => '0F5SDZRAMTBS2V3RP0ECJSE2AVIGDXC5FA42XDDG341EYBFB', :api_version => '20120505')
+    
+    #retrieve info about restaurant
     @ref = Restaurant.find_by(:id => params[:id]).ref
-    venues = client.venue(@ref)
-    @results = venues
-
+    @results = client.venue(@ref)
     @name = @results["name"]
+    @price = @results["attributes"]["groups"][0]["summary"]
     @hours = client.venue_hours(@ref)
   
-      
+    #build fields for new review  
     @restaurant = Restaurant.new
     @restaurant.reviews.build
     
+    #find existing reviews
     @review_list = Restaurant.find_by(ref: @ref)
 
+    #could have >= 0 reviews
     if @review_list.nil?
       @review_items = []
     else      
@@ -31,45 +34,69 @@ class RestaurantsController < ApplicationController
     end
   end  
   
+  def results
+    
+  end
+    
+  
   def search 
     client = Foursquare2::Client.new(:client_id => 'IGHYS4WU2EMECJYBR5E3YLPVOEKWLVA4JUQFJHG4Q1A3IGOF', :client_secret => '0F5SDZRAMTBS2V3RP0ECJSE2AVIGDXC5FA42XDDG341EYBFB', :api_version => '20120505')
 
-    query = params[:query]        
-    location = Geocoder.search(params[:location])
-    @lat = location[0].data["geometry"]["location"]["lat"]
-    @lng = location[0].data["geometry"]["location"]["lng"]
-    formatted_address = location.first.data["geometry"]["formatted_address"]
-
-
-    venues = client.search_venues(:ll => "#{@lat},#{@lng}", :query => query, :categoryId => "4d4b7105d754a06374d81259,4d4b7105d754a06376d81259", :intent => "checkin", :radius => "3000")
-    @results = venues["venues"]
+    #retrieve lat and lng
+    @query = params[:query]        
+    @location = Geocoder.search(params[:location])
     
-    if @results.nil?
-      redirect_to root_path
+    #if location.length > 0, then partial match
+    if @location.empty? or @location.length > 1 and params[:commit] == "Search"
+      #list all potential addresses and let user pick
+    
+      render 'results'
     else
-      @hash = Gmaps4rails.build_markers(@results) do |result, marker|
-        marker.lat result["location"]["lat"]
-        marker.lng result["location"]["lng"]
-        marker.infowindow "<div style='width: 150px; height: 100px;'>#{result['name']}<br>#{result['location']['address']}<br>#{result['location']['city']} #{result['location']['state']}<br><a href='/view?ref=#{result['id']}'>Show more!</a></div>"
+    
+      @lat = @location.first.data["geometry"]["location"]["lat"]
+      @lng = @location.first.data["geometry"]["location"]["lng"]
+  
+      venues = client.search_venues(:ll => "#{@lat},#{@lng}", :query => @query, :categoryId => "4d4b7105d754a06374d81259,4d4b7105d754a06376d81259", :intent => "checkin", :radius => "3000")
+      @results = venues["venues"]
+      #@price = @results["attributes"]["groups"][0]["summary"]
+    
+      #if results.length = 0, then no entries
+    
+      if @results.nil?
+        redirect_to root_path
+      else
+        #defaults to 0,0 is no markers
+        @hash = Gmaps4rails.build_markers(@results) do |result, marker|
+          marker.lat result["location"]["lat"]
+          marker.lng result["location"]["lng"]
+          marker.infowindow "<div style='width: 150px; height: 100px;'>#{result['name']}<br>#{result['location']['address']}<br>#{result['location']['city']} #{result['location']['state']}<br><a href='/view?ref=#{result['id']}'>More info</a></div>"
+        end
       end
     end
   end  
 
-  def view        
+  def view      
+    #find restaurant by ref parameter  
     @ref = params[:ref]  
     @restaurant = Restaurant.find_by(ref: @ref)
     
+    #redirect to restaurant#show with full path
     unless @restaurant.nil?
       redirect_to restaurant_path(@restaurant)
     end
     
     client = Foursquare2::Client.new(:client_id => 'IGHYS4WU2EMECJYBR5E3YLPVOEKWLVA4JUQFJHG4Q1A3IGOF', :client_secret => '0F5SDZRAMTBS2V3RP0ECJSE2AVIGDXC5FA42XDDG341EYBFB', :api_version => '20120505')      
+    
+    #retrieve info about restaurant
     @results = client.venue(@ref)
     @name = @results["name"]
+    @price = @results["attributes"]["groups"][0]["summary"]
     @hours = client.venue_hours(@ref)
+    
+    #find existing reviews
     @restaurant = Restaurant.new
     @restaurant.reviews.build
-    @review_items = []
+    @review_items = [] #equals nil since not redirected to show action
   end
   
   def new
@@ -85,14 +112,14 @@ class RestaurantsController < ApplicationController
   end
   
   def create
-    @restaurant = Restaurant.find_by(ref: restaurant_params[:ref]) 
+    @restaurant = Restaurant.find_by(ref: restaurant_params[:ref]) #check if restaurant exists
 
     if @restaurant.nil?
       @restaurant = Restaurant.new(restaurant_params) 
-      @restaurant.save   
+      @restaurant.save #save new restaurant and review
     else
-      @review = @restaurant.reviews.new(restaurant_params[:reviews_attributes]["0"])      
-      @review.save
+      @review = @restaurant.reviews.new(restaurant_params[:reviews_attributes]["0"])
+      @review.save #save review only
     end
     
     redirect_to @restaurant
--- app/helpers/static_map_helper.rb
+++ app/helpers/static_map_helper.rb
@@ -10,7 +10,7 @@ params = {
 }.merge(options)
  
 query_string = params.map{|k,v| "#{k}=#{v}"}.join("&")
-image_tag "http://maps.googleapis.com/maps/api/staticmap?#{query_string}&style=feature%3Awater%7Celement%3Aall%7Cvisibility%3Aon%7Ccolor%3A0xb5cbe4%7C&style=feature%3Alandscape%7Celement%3Aall%7Ccolor%3A0xefefef%7C&style=feature%3Aroad.highway%7Celement%3Ageometry%7Ccolor%3A0x83a5b0%7C&style=feature%3Aroad.arterial%7Celement%3Ageometry%7Ccolor%3A0xbdcdd3%7C&style=feature%3Aroad.local%7Celement%3Ageometry%7Ccolor%3A0xffffff%7C&style=feature%3Apoi.park%7Celement%3Ageometry%7Ccolor%3A0xe3eed3%7C&style=feature%3Aadministrative%7Celement%3Aall%7Cvisibility%3Aon%7Clightness%3A33%7C&style=&style=feature%3Apoi.park%7Celement%3Alabels%7Cvisibility%3Aon%7Clightness%3A20%7C&style=&style=feature%3Aroad%7Celement%3Aall%7Clightness%3A20%7C"
+image_tag("http://maps.googleapis.com/maps/api/staticmap?#{query_string}&style=feature%3Awater%7Celement%3Aall%7Cvisibility%3Aon%7Ccolor%3A0xb5cbe4%7C&style=feature%3Alandscape%7Celement%3Aall%7Ccolor%3A0xefefef%7C&style=feature%3Aroad.highway%7Celement%3Ageometry%7Ccolor%3A0x83a5b0%7C&style=feature%3Aroad.arterial%7Celement%3Ageometry%7Ccolor%3A0xbdcdd3%7C&style=feature%3Aroad.local%7Celement%3Ageometry%7Ccolor%3A0xffffff%7C&style=feature%3Apoi.park%7Celement%3Ageometry%7Ccolor%3A0xe3eed3%7C&style=feature%3Aadministrative%7Celement%3Aall%7Cvisibility%3Aon%7Clightness%3A33%7C&style=&style=feature%3Apoi.park%7Celement%3Alabels%7Cvisibility%3Aon%7Clightness%3A20%7C&style=&style=feature%3Aroad%7Celement%3Aall%7Clightness%3A20%7C", alt: "location")
 end
  
 end
